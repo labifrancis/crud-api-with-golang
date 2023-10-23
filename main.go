c363e80c52cd5db46ddbdf8ab7f6ae32fac56540
Francis Labi <moodyblerd@.com>
2023-10-23 21:45:34 +0100

    added updateMovie function

--- main.go
+++ main.go
@@ -67,8 +67,36 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 func createMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var movie Movie
-	_ = json.NewEncoder(r.Body).Decode(&movie)
+	_ = json.NewDecoder(r.Body).Decode(&movie)
 	movie.ID = strconv.Itoa(rand.Intn(100000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+
+		if item.ID == params["id"] {
+
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+
+	}
+
+	// we are declaring a movie variable of TYPE  Movie which is a struct
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
 	movies = append(movies, movie)
 	json.NewEncoder(w).Encode(movie)
 

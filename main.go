0011807a24ba65b2416048dd1db4f6ac316a2e77
Francis Labi <moodyblerd@.com>
2023-10-23 20:26:42 +0100

    added getMovie function

--- main.go
+++ main.go
@@ -25,23 +25,39 @@ type Director struct {
 var movies []Movie
 
 func getMovies(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-Type", "Application/json")
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(movies)
 
 }
 
-func deleteMovie (w http.ResponseWriter, r *http.Request){
-	w.Header().Set("content-Type", "Application/json")
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 
-	params:= mux.Vars(r)
+	params := mux.Vars(r)
 
-	for index, item:= range movies {
+	for index, item := range movies {
 
-		if item.ID = params["id"]{
-			movies = append(movies[:index],movies[index+1:]... )
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
 			break
 		}
 
+	}
+	json.NewEncoder(w).Encode(movies)
+
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	// we used a blank identifier _ instead of index because we wouldn't be using index
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+
+		}
 	}
 
 }
